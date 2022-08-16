b5f9ecd0988389655f23e41208d4eb49142063d6
Ellie <EllieRHudson@gmail.com>
2022-08-16 21:29:44 +0100

    myfree

--- src/8440.c
+++ src/8440.c
@@ -4,7 +4,12 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/8440/func_8002D0CC.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/8440/func_8002D580.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/8440/func_8002D580.s")
+extern s32 gzip_data_0000;
+
+void myfree(void) {
+    gzip_data_0000 = 0;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/8440/func_8002D58C.s")
 
