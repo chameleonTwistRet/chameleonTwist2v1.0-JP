2d4d47de6e9b9949579e52156b8f130286aa09eb
Ellie <EllieRHudson@gmail.com>
2022-08-17 12:55:50 +0100

    8440 4/6

--- include/structs.h
+++ include/structs.h
@@ -9,3 +9,13 @@ typedef struct unk_func_80040C10 {
     s16 unk0;
     s16 unk2;
 } unk_func_80040C10;
+
+typedef struct unk_80163030 {
+    char unk0[0x47];
+    f32 unk48;
+    f32 unk4C;
+    f32 unk50;
+    f32 unk54;
+    f32 unk58;
+    f32 unk5C;
+}unk_80163030 ;
