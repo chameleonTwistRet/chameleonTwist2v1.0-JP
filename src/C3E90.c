20ed09185dddb5277818062ff8a67ad181b91ef8
Ellie <EllieRHudson@gmail.com>
2022-08-18 22:18:51 +0100

    quick change

--- src/C3E90.c
+++ src/C3E90.c
@@ -1,3 +1,6 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/C3E90/func_800E8A90.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/C3E90/func_800E8A90.s")
+void func_800E8A90(s32* arg0) {
+    *arg0 = 0;
+}
