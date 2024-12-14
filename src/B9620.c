de7e605c735f8d9ba9629e33ff2f6bccb4deaa30
Rainchus <rainmachus@gmail.com>
2024-12-14 06:05:12 -0600

    even more symbols

--- src/B9620.c
+++ src/B9620.c
@@ -2,8 +2,8 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/B9620/osCreatePiManager.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/B9620/func_800DE390.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/B9620/__osPiCreateAccessQueue.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/B9620/func_800DE3E0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/B9620/__osPiGetAccess.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/B9620/func_800DE424.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/B9620/__osPiRelAccess.s")
--- src/BF5D0.c
+++ src/BF5D0.c
@@ -2,4 +2,4 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/BF5D0/func_800E41D0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/BF5D0/func_800E42F0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/BF5D0/osCartRomInit.s")
